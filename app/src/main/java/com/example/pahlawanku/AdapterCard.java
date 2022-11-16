12a382e888d84359ddaa3be14ade40e1688db097
MMauladii <muhammadmauladi@mhs.mdp.ac.id>
2022-11-16 14:24:48 +0700

    uy

--- app/src/main/java/com/example/pahlawanku/AdapterCard.java
+++ app/src/main/java/com/example/pahlawanku/AdapterCard.java
@@ -1,13 +1,42 @@
 package com.example.pahlawanku;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AdapterCard {
+import java.util.ArrayList;
+
+public class AdapterCard extends RecyclerView.Adapter<AdapterCard.classViewHolder> {
+    private ArrayList<ModelPahlawan> dataPahlawan;
+
+    public AdapterCard(ArrayList<ModelPahlawan> dataPahlawan) {
+        this.dataPahlawan = dataPahlawan;
+    }
+
+    @NonNull
+    @Override
+    public classViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View varView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
+        return new classViewHolder(varView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull classViewHolder holder, int position) {
+        ModelPahlawan pahlawan = dataPahlawan.get(position);
+        holder.tvNama.setText(pahlawan.getNama());
+        holder.tvTentang.setText(pahlawan.getTentang());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataPahlawan.size();
+    }
 
     public class classViewHolder extends RecyclerView.ViewHolder {
         ImageView ivFoto;
